3c3b602b5af22bc7c193c807a0f9a94fff8bb3d2
kgoyal <kgoyal@splunk.com>
2021-07-08 12:40:31 +0530

    custom view for runquery and get results

--- bigquery_view.py
+++ bigquery_view.py
@@ -8,59 +8,28 @@
 #
 # --
 
-from django.http import HttpResponse
-import json
-
 
 def display_query_results(provides, all_results, context):
 
-    headers = []
+    context['results'] = results = []
+    adjusted_names = {}
 
-    headers_set = set()
     for summary, action_results in all_results:
         for result in action_results:
-            job_id = result.get_summary().get('job_id')
-            if job_id is not None:
-                headers = ['job_id']
-                break
-            header_data = result.get_data()
-            if header_data:
-                headers += header_data[0].keys()
-                break
-
-    if not headers_set:
-        headers_set.update(headers)
-    headers = sorted(headers_set)
-
-    context['ajax'] = True
-    if 'start' not in context['QS']:
-        context['headers'] = headers
-        return '/widgets/generic_table.html'
-
-    start = int(context['QS']['start'][0])
-    length = int(context['QS'].get('length', ['5'])[0])
-    end = start + length
-    # cur_pos = 0
-    rows = []
-    total = 0
-    for summary, action_results in all_results:
-        for result in action_results:
-            job_id = result.get_summary().get('job_id')
-            if job_id is not None:
-                rows = [ [ {'value': job_id, 'contains': [ 'bigquery job id' ] } ] ]
-                break
+            headers_set = set()
+            table = dict()
+            table['data'] = rows = []
+            
             data = result.get_data()
-            total += len(data)
-            for item in data[start:end]:
+            if data:
+                headers_set.update(data[0].keys())
+            headers = sorted(headers_set)
+            table['headers'] = headers
+            for item in data:
                 row = []
                 for h in headers:
-                    row.append({ 'value': item.get(h) })
+                    row.append({ 'value': item.get(adjusted_names.get(h, h)) })
                 rows.append(row)
+            results.append(table)
 
-    content = {
-        "data": rows,
-        "recordsTotal": total,
-        "recordsFiltered": total,
-    }
-
-    return HttpResponse(json.dumps(content), content_type='text/javascript')
+    return 'bigquery_run_query.html'

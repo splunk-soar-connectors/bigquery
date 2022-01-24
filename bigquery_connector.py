849e286c99a02c3be28468b1337d1654f99a9694
knawattranakul <knawattranakul@splunk.com>
2022-01-24 15:12:39 -0800

    fix semgrep

--- bigquery_connector.py
+++ bigquery_connector.py
@@ -24,6 +24,7 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 from phantom.action_result import ActionResult
 from phantom.base_connector import BaseConnector
+from bigquery_consts import *
 
 # "What's going on here?"
 # Wheel files don't install how you expect (want?) them to if they would all go into the same directory
@@ -249,12 +250,14 @@ if __name__ == '__main__':
     argparser.add_argument('input_test_json', help='Input Test JSON file')
     argparser.add_argument('-u', '--username', help='username', required=False)
     argparser.add_argument('-p', '--password', help='password', required=False)
+    argparser.add_argument('-v', '--verify', action='store_true', help='verify', required=False, default=False)
 
     args = argparser.parse_args()
     session_id = None
 
     username = args.username
     password = args.password
+    verify = args.verify
 
     if username is not None and password is None:
 
@@ -265,7 +268,7 @@ if __name__ == '__main__':
     if username and password:
         try:
             print("Accessing the Login page")
-            r = requests.get("{}login".format(BaseConnector._get_phantom_base_url()), verify=False)
+            r = requests.get("{}login".format(BaseConnector._get_phantom_base_url()), verify=verify, timeout=DEFAULT_TIMEOUT)
             csrftoken = r.cookies['csrftoken']
 
             data = dict()
@@ -278,7 +281,9 @@ if __name__ == '__main__':
             headers['Referer'] = "{}login".format(BaseConnector._get_phantom_base_url())
 
             print("Logging into Platform to get the session id")
-            r2 = requests.post("{}login".format(BaseConnector._get_phantom_base_url()), verify=False, data=data, headers=headers)
+            r2 = requests.post(
+                "{}login".format(BaseConnector._get_phantom_base_url()),
+                verify=verify, data=data, headers=headers, timeout=DEFAULT_TIMEOUT)
             session_id = r2.cookies['sessionid']
         except Exception as e:
             print("Unable to get session id from the platfrom. Error: " + str(e))
@@ -286,7 +291,7 @@ if __name__ == '__main__':
 
     if len(sys.argv) < 2:
         print("No test json specified as input")
-        exit(0)
+        sys.exit(0)
 
     with open(sys.argv[1]) as f:
         in_json = f.read()
@@ -302,4 +307,4 @@ if __name__ == '__main__':
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
 
-    exit(0)
+    sys.exit(0)
